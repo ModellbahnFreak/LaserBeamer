a67c8163efe17951227f366d316f487124ec8a8e
ModellbahnFreak <freak.mdellbahn@gmail.com>
2018-10-15 22:43:44 +0200

    Some changes to NodeToString etc

--- src/sequence/NodeCreator.java
+++ src/sequence/NodeCreator.java
@@ -12,6 +12,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -28,6 +29,7 @@ public class NodeCreator {
 			return true;
 		case "vid":
 			return true;
+		case "txtSize":
 		case "txt":
 			return true;
 		case "line":
@@ -49,6 +51,7 @@ public class NodeCreator {
 			return createImg(ToDos);
 		case "vid":
 			return createVid(ToDos, VoidDel);
+		case "txtSize":
 		case "txt":
 			return showText(ToDos, cmd);
 		case "line":
@@ -69,12 +72,12 @@ public class NodeCreator {
 			if (toDos.length >= 6) {
 				kreis = new Circle();
 				kreis.setId(toDos[1]);
-				kreis.setCenterX(Double.parseDouble(toDos[2]));
-				kreis.setCenterY(Double.parseDouble(toDos[3]));
+				kreis.setCenterX(Double.parseDouble(toDos[2])*Gui.INSTANCE.scene.getWidth());
+				kreis.setCenterY(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getHeight());
 				if ("full".equals(toDos[4])) {
 					kreis.setRadius(Gui.INSTANCE.scene.getWidth() - kreis.getCenterX());
 				} else {
-					kreis.setRadius(Double.parseDouble(toDos[4]));
+					kreis.setRadius(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth());
 				}
 				kreis.setFill(Color.web(toDos[5]));
 				kreis.setStroke(Color.WHITE);
@@ -82,7 +85,7 @@ public class NodeCreator {
 			}
 			if (toDos.length >= 8) {
 				kreis.setStroke(Color.web(toDos[6]));
-				kreis.setStrokeWidth(Double.parseDouble(toDos[7]));
+				kreis.setStrokeWidth(Double.parseDouble(toDos[7])*Gui.INSTANCE.scene.getWidth());
 			}
 			return kreis;
 		} catch (NumberFormatException e) {
@@ -96,29 +99,29 @@ public class NodeCreator {
 			if (toDos.length >= 7) {
 				rect = new Rectangle();
 				rect.setId(toDos[1]);
-				rect.setX(Double.parseDouble(toDos[2]));
-				rect.setY(Double.parseDouble(toDos[3]));
+				rect.setX(Double.parseDouble(toDos[2])*Gui.INSTANCE.scene.getWidth());
+				rect.setY(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getHeight());
 				if ("full".equals(toDos[4])) {
 					rect.setWidth(Gui.INSTANCE.scene.getWidth());
 				} else {
-					rect.setWidth(Double.parseDouble(toDos[4]));
+					rect.setWidth(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth());
 				}
 				if ("full".equals(toDos[5])) {
 					rect.setHeight(Gui.INSTANCE.scene.getHeight());
 				} else {
-					rect.setHeight(Double.parseDouble(toDos[5]));
+					rect.setHeight(Double.parseDouble(toDos[5])*Gui.INSTANCE.scene.getHeight());
 				}
 				rect.setFill(Color.web(toDos[6]));
 				rect.setStroke(Color.WHITE);
 				rect.setStrokeWidth(0);
 			}
 			if (toDos.length >= 9) {
-				rect.setArcWidth(Double.parseDouble(toDos[7]));
-				rect.setArcHeight(Double.parseDouble(toDos[8]));
+				rect.setArcWidth(Double.parseDouble(toDos[7])*rect.getWidth());
+				rect.setArcHeight(Double.parseDouble(toDos[8])*rect.getHeight());
 			}
 			if (toDos.length >= 11) {
 				rect.setStroke(Color.web(toDos[9]));
-				rect.setStrokeWidth(Double.parseDouble(toDos[10]));
+				rect.setStrokeWidth(Double.parseDouble(toDos[10])*Gui.INSTANCE.scene.getWidth());
 			}
 			return rect;
 		} catch (NumberFormatException e) {
@@ -132,15 +135,15 @@ public class NodeCreator {
 			if (toDos.length >= 7) {
 				linie = new Line();
 				linie.setId(toDos[1]);
-				linie.setStartX(Double.parseDouble(toDos[2]));
-				linie.setStartY(Double.parseDouble(toDos[3]));
-				linie.setEndX(Double.parseDouble(toDos[4]));
-				linie.setEndY(Double.parseDouble(toDos[5]));
+				linie.setStartX(Double.parseDouble(toDos[2])*Gui.INSTANCE.scene.getWidth());
+				linie.setStartY(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getHeight());
+				linie.setEndX(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth());
+				linie.setEndY(Double.parseDouble(toDos[5])*Gui.INSTANCE.scene.getHeight());
 				linie.setStroke(Color.web(toDos[6]));
 				// linie.setStrokeWidth(10);
 			}
 			if (toDos.length == 8) {
-				linie.setStrokeWidth(Double.parseDouble(toDos[7]));
+				linie.setStrokeWidth(Double.parseDouble(toDos[7])*Gui.INSTANCE.scene.getWidth());
 			}
 			return linie;
 		} catch (NumberFormatException e) {
@@ -170,28 +173,28 @@ public class NodeCreator {
 				String mode = "";
 				if (toDos.length == 3) {
 					mediaView.setX(0);
-					mediaView.setX(0);
+					mediaView.setY(0);
 				} else if (toDos.length == 4) {
 					mediaView.setX(0);
-					mediaView.setX(0);
+					mediaView.setY(0);
 					mode = toDos[3];
 				} else if (toDos.length == 6) {
 					mode = toDos[3];
-					mediaView.setX(Double.parseDouble(toDos[4]));
-					mediaView.setX(Double.parseDouble(toDos[5]));
+					mediaView.setX(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth());
+					mediaView.setY(Double.parseDouble(toDos[5])*Gui.INSTANCE.scene.getHeight());
 				} else if (toDos.length == 8) {
 					mode = toDos[3];
-					mediaView.setX(Double.parseDouble(toDos[4]));
-					mediaView.setX(Double.parseDouble(toDos[5]));
+					mediaView.setX(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth());
+					mediaView.setY(Double.parseDouble(toDos[5])*Gui.INSTANCE.scene.getHeight());
 					if ("full".equals(toDos[6])) {
 						mediaView.setFitWidth(Gui.INSTANCE.scene.getWidth());
 					} else {
-						mediaView.setFitWidth(Double.parseDouble(toDos[6]));
+						mediaView.setFitWidth(Double.parseDouble(toDos[6])*Gui.INSTANCE.scene.getWidth());
 					}
 					if ("full".equals(toDos[7])) {
 						mediaView.setFitHeight(Gui.INSTANCE.scene.getHeight());
 					} else {
-						mediaView.setFitHeight(Double.parseDouble(toDos[7]));
+						mediaView.setFitHeight(Double.parseDouble(toDos[7])*Gui.INSTANCE.scene.getHeight());
 					}
 				} else {
 					return null;
@@ -275,22 +278,22 @@ public class NodeCreator {
 				BildView = new ImageView();
 				if (toDos.length == 3) {
 					BildView.setX(0);
-					BildView.setX(0);
+					BildView.setY(0);
 				} else if (toDos.length == 5) {
-					BildView.setX(Double.parseDouble(toDos[3]));
-					BildView.setX(Double.parseDouble(toDos[4]));
+					BildView.setX(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getWidth());
+					BildView.setY(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getHeight());
 				} else if (toDos.length == 7) {
-					BildView.setX(Double.parseDouble(toDos[3]));
-					BildView.setX(Double.parseDouble(toDos[4]));
+					BildView.setX(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getWidth());
+					BildView.setY(Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getHeight());
 					if ("full".equals(toDos[5])) {
 						BildView.setFitWidth(Gui.INSTANCE.scene.getWidth());
 					} else {
-						BildView.setFitWidth(Double.parseDouble(toDos[5]));
+						BildView.setFitWidth(Double.parseDouble(toDos[5])*Gui.INSTANCE.scene.getWidth());
 					}
 					if ("full".equals(toDos[6])) {
 						BildView.setFitHeight(Gui.INSTANCE.scene.getHeight());
 					} else {
-						BildView.setFitHeight(Double.parseDouble(toDos[6]));
+						BildView.setFitHeight(Double.parseDouble(toDos[6])*Gui.INSTANCE.scene.getHeight());
 					}
 				} else {
 					return null;
@@ -311,18 +314,26 @@ public class NodeCreator {
 		try {
 			int textStart = sendData.indexOf("'") + 1;
 			int textEnd = sendData.lastIndexOf("'");
-			String text = sendData.substring(textStart, textEnd);
-			if (text.length() <= 0 || text.isEmpty()) {
+			String text = "";
+			if (textStart < 0 || textEnd < 0) {
 				text = toDos[4];
+			} else {
+				text = sendData.substring(textStart, textEnd);
 			}
 			if (text.length() <= 0 || text.isEmpty()) {
 				text = toDos[1];
 			}
+			double textSize = 50;
+			String font = "Ravie";
+			if ("txtSize".equals(toDos[0])) {
+				textSize = Double.parseDouble(toDos[4])*Gui.INSTANCE.scene.getWidth();
+				font = toDos[5];
+			}
 			Text Ausg = new Text(text);
 			Ausg.setFill(Color.web(toDos[toDos.length - 1]));
-			Ausg.setX(Double.parseDouble(toDos[2]));
-			Ausg.setY(Double.parseDouble(toDos[3]));
-			Ausg.setFont(Font.font("Ravie", FontWeight.NORMAL, 50));
+			Ausg.setX(Double.parseDouble(toDos[2])*Gui.INSTANCE.scene.getWidth());
+			Ausg.setY(Double.parseDouble(toDos[3])*Gui.INSTANCE.scene.getHeight());
+			Ausg.setFont(Font.font(font, FontWeight.NORMAL, textSize));
 			Ausg.setId(toDos[1]);
 			return Ausg;
 		} catch (NumberFormatException e) {
@@ -332,30 +343,69 @@ public class NodeCreator {
 	
 	public static String nodeToString (Node objekt) {
 		String erg = "";
+		double sceneW = Gui.INSTANCE.scene.getWidth(),
+				sceneH = Gui.INSTANCE.scene.getHeight();
 		if (objekt instanceof ImageView) {
-			erg = "img;" + objekt.getId() + ";" + objekt.getProperties().get("path").toString() + ";" + ((ImageView)objekt).getX() + ";" + ((ImageView)objekt).getY() + ";" + ((ImageView)objekt).getFitWidth() + ";" + ((ImageView)objekt).getFitHeight();
+			double xPos = ((ImageView)objekt).getX()/sceneW,
+				yPos = ((ImageView)objekt).getY()/sceneH,
+				width = ((ImageView)objekt).getFitWidth()/sceneW,
+				height = ((ImageView)objekt).getFitHeight()/sceneH;
+			erg = "img;" + objekt.getId() + ";" + objekt.getProperties().get("path").toString() + ";" + xPos + ";" + yPos + ";" + width + ";" + height;
 		} else if (objekt instanceof MediaView) {
-			erg = "vid;" + objekt.getId() + ";" + objekt.getProperties().get("path").toString() + ";" + objekt.getProperties().get("mode") + ";" + ((MediaView)objekt).getX() + ";" + ((MediaView)objekt).getY() + ";" + ((MediaView)objekt).getFitWidth() + ";" + ((MediaView)objekt).getFitHeight();
+			double xPos = ((MediaView)objekt).getX()/sceneW,
+					yPos = ((MediaView)objekt).getY()/sceneH,
+					width = ((MediaView)objekt).getFitWidth()/sceneW,
+					height = ((MediaView)objekt).getFitHeight()/sceneH;
+			erg = "vid;" + objekt.getId() + ";" + objekt.getProperties().get("path").toString() + ";" + objekt.getProperties().get("mode") + ";" + xPos + ";" + yPos + ";" + width + ";" + height;
 		} else if (objekt instanceof Text) {
-			erg = "txt;" + objekt.getId() + ";" + ((Text)objekt).getX() + ";" + ((Text)objekt).getY() + ";'" + ((Text)objekt).getText() + "';" + ((Text)objekt).getFill().toString();
+			double xPos = ((Text)objekt).getX()/sceneW,
+					yPos = ((Text)objekt).getY()/sceneH,
+					size = ((Text)objekt).getFont().getSize()/sceneW;
+			String fontFamily = ((Text)objekt).getFont().getFamily();
+			erg = "txtSize;" + objekt.getId() + ";" + xPos + ";" + yPos + ";" + size + ";" + fontFamily + ";'" + ((Text)objekt).getText() + "';" + colorToStr(((Text) objekt).getFill());
 		} else if (objekt instanceof Line) {
-			erg = "line;" + objekt.getId() + ";" + ((Line)objekt).getStartX() + ";" + ((Line)objekt).getStartY() + ";" + ((Line)objekt).getEndX() + ";" + ((Line)objekt).getEndY() + ";" + ((Line)objekt).getStroke().toString() + ";" + ((Line)objekt).getStrokeWidth();
+			double x1 = ((Line)objekt).getStartX()/sceneW,
+					y1 = ((Line)objekt).getStartY()/sceneH,
+					x2 = ((Line)objekt).getEndX()/sceneW,
+					y2 = ((Line)objekt).getEndY()/sceneH,
+					strokeW = ((Line)objekt).getStrokeWidth()/sceneW;
+			erg = "line;" + objekt.getId() + ";" + x1 + ";" + y1 + ";" + x2 + ";" + y2 + ";" + colorToStr(((Line)objekt).getStroke()) + ";" + strokeW;
 		} else if (objekt instanceof Rectangle) {
 			String id = objekt.getId();
-			String xpos = String.valueOf(((Rectangle)objekt).getX());
-			String ypos = String.valueOf(((Rectangle)objekt).getY());
-			String w = String.valueOf(((Rectangle)objekt).getWidth());
-			String h = String.valueOf(((Rectangle)objekt).getHeight());
+			double xpos = ((Rectangle)objekt).getX()/sceneW;
+			double ypos = ((Rectangle)objekt).getY()/sceneH;
+			double w = ((Rectangle)objekt).getWidth()/sceneW;
+			double h = ((Rectangle)objekt).getHeight()/sceneH;
 			String color = String.valueOf(((Rectangle)objekt).getFill());
-			String ArcW = String.valueOf(((Rectangle)objekt).getArcWidth());
-			String ArcH = String.valueOf(((Rectangle)objekt).getArcHeight());
-			String StrokeC = String.valueOf(((Rectangle)objekt).getStroke());
-			String StrokeW = String.valueOf(((Rectangle)objekt).getStrokeWidth());
+			double ArcW = ((Rectangle)objekt).getArcWidth()/w;
+			double ArcH = ((Rectangle)objekt).getArcHeight()/h;
+			String StrokeC = colorToStr(((Rectangle)objekt).getStroke());
+			double StrokeW = ((Rectangle)objekt).getStrokeWidth()/sceneW;
 			erg = "rect;" + id + ";" + xpos + ";" + ypos + ";" + w + ";" + h + ";" + color + ";" + ArcW + ";" + ArcH + ";" + StrokeC + ";" + StrokeW;
 			//erg = "rect;" + objekt.getId() + ";" + ((Rectangle)objekt).getX() + ";" + ((Rectangle)objekt).getY() + ";" + ((Rectangle)objekt).getWidth() + ";" + ((Rectangle)objekt).getHeight() + ";" + ((Rectangle)objekt).getFill().toString() + ";" + ((Rectangle)objekt).getArcWidth() + ";" + ((Rectangle)objekt).getArcHeight() + ";" + ((Rectangle)objekt).getStroke().toString() + ";" + ((Rectangle)objekt).getStrokeWidth();
 		} else if (objekt instanceof Circle) {
-			erg = "circle;" + objekt.getId() + ";" + ((Circle)objekt).getCenterX() + ";" + ((Circle)objekt).getCenterY() + ";" + ((Circle)objekt).getRadius() + ";" + ((Circle)objekt).getFill().toString() + ";" + ((Circle)objekt).getStroke().toString() + ";" + ((Circle)objekt).getStrokeWidth();
+			double xPos = ((Circle)objekt).getCenterX()/sceneW,
+					yPos = ((Circle)objekt).getCenterY()/sceneH,
+					size = ((Circle)objekt).getRadius()/sceneW,
+					strokeW = ((Circle)objekt).getStrokeWidth()/sceneW;
+			erg = "circle;" + objekt.getId() + ";" + xPos + ";" + yPos + ";" + size + ";" + colorToStr(((Circle)objekt).getFill()) + ";" + colorToStr(((Circle)objekt).getStroke()) + ";" + strokeW;
 		}
 		return erg;
+	}
+	
+	public static String getAllObjTypes() {
+		String erg = "txt-Text;" + 
+				"txtSize-Text individuell;" + 
+				"img-Bild;" + 
+				"vid-Video;" + 
+				"line-Linie;" + 
+				"rect-Rechteck/Oval;" + 
+				"circle-Kreis";
+		return erg;
+	}
+	
+	private static String colorToStr(Paint paint) {
+		Color c = (Color) paint;
+		return String.format( "#%02X%02X%02X", (int)( c.getRed() * 255 ), (int)( c.getGreen() * 255 ), (int)( c.getBlue() * 255 ) );
 	}
 }
